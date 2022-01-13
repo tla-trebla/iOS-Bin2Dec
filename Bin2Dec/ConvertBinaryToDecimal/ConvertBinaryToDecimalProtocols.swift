84123974487e0137bfaa9aa2be1b3f66f1174424
Albert Pangestu <stamina.left@gmail.com>
2022-01-14 01:23:33 +0700

    Created PresenterInput, View, and Router protocol.

--- Bin2Dec/ConvertBinaryToDecimal/ConvertBinaryToDecimalProtocols.swift
+++ Bin2Dec/ConvertBinaryToDecimal/ConvertBinaryToDecimalProtocols.swift
@@ -7,6 +7,24 @@
 
 import Foundation
 
+protocol ConvertBinaryToDecimalPresenterInputProtocol {
+    
+    var view: ConvertBinaryToDecimalViewProtocol? { get set }
+    var interactor: ConvertBinaryToDecimalInteractorProtocol? { get set }
+    var router: ConvertBinaryToDecimalRouter? { get set }
+    
+    func presentConvertBinaryToDecimal(_ request: String)
+}
+
+protocol ConvertBinaryToDecimalViewProtocol {
+    func showDecimal(decimal: Int)
+    func showError(message: String)
+}
+
+protocol ConvertBinaryToDecimalRouter {
+    func createModule() -> ViewController
+}
+
 protocol ConvertBinaryToDecimalPresenterOutputProtocol {
     func displayDecimal(response decimal: Int)
     func displayFailure(message: String)
