3a35287f18ca47d2b66c18ad26e9cae138548066
Albert Pangestu <stamina.left@gmail.com>
2022-01-13 21:39:44 +0700

    Created new Binary model with numeral property and convertToDecimal method.

---
+++ Bin2Dec/Entity/BinaryModel.swift
@@ -0,0 +1,17 @@
+//
+//  BinaryModel.swift
+//  Bin2Dec
+//
+//  Created by Albert Pangestu on 13/01/22.
+//
+
+import Foundation
+
+struct Binary {
+    
+    var numeral = 0
+    
+    func convertToDecimal() -> Int {
+        return 0
+    }
+}
