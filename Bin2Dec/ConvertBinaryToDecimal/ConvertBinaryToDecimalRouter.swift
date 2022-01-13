622a7cf0666921e42122c8810f20b848b62644cc
Albert Pangestu <stamina.left@gmail.com>
2022-01-14 02:49:17 +0700

    Implemented ConvertBinaryToDecimalRouter with createModule implementation.

--- Bin2Dec/ConvertBinaryToDecimal/ConvertBinaryToDecimalRouter.swift
+++ Bin2Dec/ConvertBinaryToDecimal/ConvertBinaryToDecimalRouter.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import UIKit
+
+class ConvertBinaryToDecimalRouter: ConvertBinaryToDecimalRouterProtocol {
+    
+    func createModule() -> ViewController {
+        
+        let view = ConvertBinaryToDecimalRouter
+            .mainStoryboard
+            .instantiateViewController(withIdentifier: "ViewController")
+        as! ViewController
+        
+        let presenter = ConvertBinaryToDecimalPresenter()
+        let interactor = ConvertBinaryToDecimalInteractor()
+        let router = ConvertBinaryToDecimalRouter()
+        
+        view.presenter = presenter
+        presenter.view = view
+        presenter.router = router
+        presenter.interactor = interactor
+        interactor.presenter = presenter
+        
+        return view
+    }
+    
+    static var mainStoryboard: UIStoryboard {
+        return UIStoryboard(name: "Main", bundle: Bundle.main)
+    }
+}
