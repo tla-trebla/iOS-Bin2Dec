9c238b3fd1d0f36b43d45ba4cbf3ef199ae1a5e8
Albert Pangestu <stamina.left@gmail.com>
2022-01-13 21:49:26 +0700

    Implemented the conversion of numeric into decimal and created updateNumeric method.

--- Bin2Dec/Entity/BinaryModel.swift
+++ Bin2Dec/Entity/BinaryModel.swift
@@ -12,6 +12,21 @@ struct Binary {
     var numeral = "0"
     
     func convertToDecimal() -> Int {
-        return 0
+        
+        var result = 0
+        
+        for digit in numeral {
+            if digit == "0" {
+                result *= 2
+            } else {
+                result = result * 2 + 1
+            }
+        }
+        
+        return result
+    }
+    
+    mutating func updateNumeric(_ value: String) {
+        self.numeral = value
     }
 }
