fbb39895639d87860d05874404fe5a0005628a45
Albert Pangestu <stamina.left@gmail.com>
2022-01-13 21:20:10 +0700

    Initial Commit

---
+++ Bin2Dec/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bin2Dec
+//
+//  Created by Albert Pangestu on 13/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
