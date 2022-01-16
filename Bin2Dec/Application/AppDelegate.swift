b52a5f8c9c895da124a6aa4547a4212c3c5160cd
Albert Pangestu <stamina.left@gmail.com>
2022-01-16 19:33:17 +0700

    Removed implementation on AppDelegate because it should be at the SceneDelegate.

--- Bin2Dec/Application/AppDelegate.swift
+++ Bin2Dec/Application/AppDelegate.swift
@@ -10,13 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        let convertBinaryToDecimal = ConvertBinaryToDecimalRouter().createModule()
-        
-        window?.rootViewController = convertBinaryToDecimal
         
         return true
     }
